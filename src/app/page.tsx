fc19f111c0588b8e70e8bb46de440c5bf99e310d
aaronjones1 <126199236+aaronjones0@users.noreply.github.com>
2023-06-20 14:53:40 -0400

    Home page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 'use client';
 
+import { LifebuoyIcon } from '@heroicons/react/24/outline';
 import HomeView from '@muze/components/HomeView/HomeView';
 import ImageCard from '@muze/components/ImageCard/ImageCard';
+import LinkButton from '@muze/components/LinkButton/LinkButton';
 import C from '@muze/components/TextFormatting/Code';
+import Link from 'next/link';
 
 export default async function Home() {
   return (
@@ -22,8 +25,18 @@ export default async function Home() {
       </p>
       <p>
         ⚡ Type <C>/v books</C> and hit <C>Enter</C> to view all your Book
-        entries.
+        entries. Also try <C>/v tvseries</C> and <C>/v manga</C>.
       </p>
+      <div className='relative shadow-nh-md w-fit rounded-full my-4'>
+        <Link
+          target='_blank'
+          href='https://windfallprojects.gitbook.io/muze'
+          className='shadow-ns-md flex flex-row gap-1.5 items-center border-t-2 border-neutral-800 hover:border-neutral-700 px-3 py-2 rounded-full text-neutral-400 hover:text-neutral-300 bg-neutral-900 hover:bg-neutral-800 w-fit'
+        >
+          <LifebuoyIcon className='h-6 w-6 text-indigo-500' />
+          Help
+        </Link>
+      </div>
     </>
   );
 }
